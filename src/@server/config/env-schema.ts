3e3b02bd2ad6b5db46d97d13685c119c29d51113
Alex Cândido <alex.candido.tec@gmail.com>
2024-02-27 12:59:36 -0300

    adicionado estrutua inicial de auth em src/@server/config/next-auth.ts

---
+++ src/@server/config/env-schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+const CONFIG_ENV_SCHEMA = z.object({
+  DATABASE_URL: z.string(),
+  NEXT_BASE_URL_API: z.string(),
+  NEXTAUTH_SECRET: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  OPENAI_API_KEY: z.string(),
+})
+
+export const env = CONFIG_ENV_SCHEMA.parse(process.env)
